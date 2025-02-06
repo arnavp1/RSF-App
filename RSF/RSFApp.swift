d172241a77cd0bc1cb7336664e07e1b54f188d09
Arnav Podichetty <arnav.p2024@gmail.com>
2025-02-05 17:58:53 -0800

    Initial Commit

---
+++ RSF/RSFApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSFApp.swift
+//  RSF
+//
+//  Created by Arnav Podichetty on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RSFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RSFTests/RSFTests.swift
@@ -0,0 +1,16 @@
+//
+//  RSFTests.swift
+//  RSFTests
+//
+//  Created by Arnav Podichetty on 2/5/25.
+//
+
+import Testing
+
+struct RSFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
